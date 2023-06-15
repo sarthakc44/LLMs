1d9d84c7c01b1e8c4e2ba175ab09e7a1caec23f1
sarthakc44 <92016609+sarthakc44@users.noreply.github.com>
2023-06-15 17:16:47 +0530

    Update main
    added st library

--- style-transfer/main.py
+++ style-transfer/main.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
+import streamlit as st
 
 model = "tiiuae/falcon-7b-instruct"
 
@@ -14,17 +15,21 @@ pipeline = transformers.pipeline(
     device_map="auto",
 )
 
-text = 'Paraphrase the following sentence delimited by curly brackets pirate english: {It is almost 2pm. Time to go eat.}'
+prompt = 'Paraphrase the following sentence delimited by curly brackets into'
+style = ' pirate english: '
+input_text = '{' + 'It is almost 2pm. Time to go eat.' + '}'
 
-# text = prompt + style + input_text
+text = prompt + style + input_text
 
 sequences = pipeline(
     text,
     max_length=200,
     do_sample=True,
     top_k=10,
-    num_return_sequences=2,
+    num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
 )
 for seq in sequences:
-    print(f"Result: {seq['generated_text']}")
+    st.markdown(f"Result: {seq['generated_text']}")
+
+    
